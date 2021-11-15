99fe53d2c61fae57790663f3662625b006f2379c
Fernando Dias Seixas <nando21.net@gmail.com>
2021-11-15 08:46:54 -0300

    Database seeding

--- backend/src/main/java/com/msxstudio/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/msxstudio/dscatalog/services/CategoryService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import com.msxstudio.dscatalog.entities.Category;
 import com.msxstudio.dscatalog.repositories.CategoryRepository;
 
@@ -14,8 +14,8 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
-		
 		return repository.findAll();
 	}
 

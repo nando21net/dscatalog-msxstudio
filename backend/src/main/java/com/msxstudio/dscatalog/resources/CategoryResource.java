7ddbd68582796e0d3e851c274cfb9855a305edad
Fernando Dias Seixas <nando21.net@gmail.com>
2021-11-15 11:35:46 -0300

    Insert Category

--- backend/src/main/java/com/msxstudio/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/msxstudio/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.msxstudio.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.msxstudio.dscatalog.dto.CategoryDTO;
 import com.msxstudio.dscatalog.services.CategoryService;
@@ -23,12 +27,19 @@ public class CategoryResource {
 	public ResponseEntity<List<CategoryDTO>> findAll() {
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
-}
-	
+	}
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
-	CategoryDTO dto = service.findById(id);
-	return ResponseEntity.ok().body(dto);
-}
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 
 }

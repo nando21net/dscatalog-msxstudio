fd81bd75b2b6a622fb65c5aabd2c8930fe671824
Fernando Dias Seixas <nando21.net@gmail.com>
2021-11-15 09:16:04 -0300

    DTO

--- backend/src/main/java/com/msxstudio/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/msxstudio/dscatalog/services/CategoryService.java
@@ -1,22 +1,28 @@
 package com.msxstudio.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.msxstudio.dscatalog.dto.CategoryDTO;
 import com.msxstudio.dscatalog.entities.Category;
 import com.msxstudio.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO > findAll(){
+		List<Category> list =  repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+			
 	}
 
 }
